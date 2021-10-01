3b8e1b237b3042ba9b34046d787a2bb076c74baa
ryopper <ryopper777@gmail.com>
2021-10-01 21:28:42 +0900

    2nd commit

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -13,7 +13,7 @@ from linebot.models import TextSendMessage
 CHANNEL_ACCESS_TOKEN = info["CHANNEL_ACCESS_TOKEN"]
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
-chart_duty = pd.read_excel("Desktop/楓Duty.xlsx")
+chart_duty = pd.read_excel("dutybot/楓Duty.xlsx")
 
 def main():
     USER_ID = info["USER_ID"]
