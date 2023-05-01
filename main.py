cafc8f70512ab36e8a405d23c15495f8a3036379
ryopper <88011195+ryopper@users.noreply.github.com>
2023-05-01 21:40:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from linebot.models import TextSendMessage
 CHANNEL_ACCESS_TOKEN = info["CHANNEL_ACCESS_TOKEN"]
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
-chart_duty = pd.read_excel("楓Duty.xlsx")
+chart_duty = pd.read_excel("新DUTY_LIST.xlsx")
 
 def main():
     USER_ID = info["USER_ID"]
