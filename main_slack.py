6b7df6dee89bd34b521145f716239cc3e43cbda9
ryopper <88011195+ryopper@users.noreply.github.com>
2023-09-13 14:54:46 +0900

    Add files via upload

---
+++ main_slack.py
@@ -0,0 +1,31 @@
+import json
+import datetime
+import pandas as pd
+import requests
+
+dt_now = datetime.datetime.now()
+
+file = open("info_slack.json","r")
+info = json.load(file)
+
+
+CHANNEL = info["CHANNEL"]
+TOKEN = info["TOKEN"]
+URL = info["URL"]
+
+chart_duty = pd.read_excel("新DUTY_LIST.xlsx")
+
+def main():
+    name = chart_duty.iloc[dt_now.day-1,1]
+    if pd.isnull(name) == False:
+        text = f'*本日のDutyは{name}さんです。*'
+        data = {
+            'token': TOKEN,
+            'channel': CHANNEL,
+            'text': text
+        }
+        response = requests.post(URL, data=data)
+
+
+if __name__ == "__main__":
+    main()
